80edb5e34e036b6ca5d587eccc70b71f79221e14
nimishverma97 <nimishverma@ymail.com>
2018-07-17 17:07:41 +0530

    Sad

--- BirthdayPostStash/BirthdayPostStash/settings.py
+++ BirthdayPostStash/BirthdayPostStash/settings.py
@@ -179,7 +179,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'BirthdayPostStash/static'),
+    os.path.join(PROJECT_ROOT, 'static'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
