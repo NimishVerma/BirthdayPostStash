caed1399ab25e0b126dff1b3bbb6106619dcee53
nimishverma97 <nimishverma@ymail.com>
2018-07-17 16:48:52 +0530

    zz

--- BirthdayPostStash/BirthdayPostStash/settings.py
+++ BirthdayPostStash/BirthdayPostStash/settings.py
@@ -167,5 +167,20 @@ MEDIA_URL = '/media/'
 
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 LOGIN_URL = '/users/login'
-STATIC_URL = '/static/'
+
 LOGIN_REDIRECT_URL = '/users/profile/'
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
